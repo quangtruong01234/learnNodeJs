d73b585ff6ee468c98108fc44de3143b36120fe3
Truong dev <quangtruong01234@gmail.com>
2024-04-01 03:08:07 +0700

    successResponse session 8

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,8 +1,15 @@
+const { CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.signUp(req.body));
+    new CREATED({
+      message:'Resisted OK!',
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit:10
+      }
+    }).send(res)
   };
 }
 
